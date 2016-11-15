9af8455d410e3d922987d765e2b2dc7fda10c612
Kelleigh Maroney <kmaroney@credera.com>
2016-11-15 11:24:47 -0600

    trying again

--- gulp-tasks/serveprod.js
+++ gulp-tasks/serveprod.js
@@ -4,7 +4,7 @@ module.exports = function(gulp, config, plugins) {
   return function() {
     plugins.connect.server({
     root: ['client/build'],
-    port: process.env.PORT || 5000, // localhost:5000
+    port: process.env.PORT || 8080, // localhost:5000
     livereload: false
   });
   };
